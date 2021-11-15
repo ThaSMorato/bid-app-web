a3465c5dcaf192c6b4853416aa5273119c17c4f2
Thales Morato <tha.s.morato@gmail.com>
2021-11-15 11:44:42 -0300

    feat: component card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,28 @@
+import { Button } from "../Button";
+import styles from "./styles.module.scss";
+
+type Product = {
+  name: string;
+  description: string;
+  image_url: string;
+  id: string;
+};
+
+type CardProps = {
+  product: Product;
+};
+
+export const Card = ({ product }: CardProps) => {
+  return (
+    <div className={styles.Card}>
+      <img src={product.image_url} alt={product.name} />
+      <div className={styles.Card__footer}>
+        <div>
+          <h4>{product.name}</h4>
+          <p>{product.description}</p>
+        </div>
+        <Button>Bid now</Button>
+      </div>
+    </div>
+  );
+};
