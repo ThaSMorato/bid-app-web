a09736e170771c30af27fb96382aa75842536cec
Thales Morato <tha.s.morato@gmail.com>
2021-11-16 08:59:02 -0300

    feat: save config

--- src/pages/Config/index.tsx
+++ src/pages/Config/index.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { Layout } from "../../components/Layout";
 import { PageLoading } from "../../components/PageLoading";
 import { useAuth } from "../../hooks/useAuth";
+import { api } from "../../service/api";
 import styles from "./styles.module.scss";
 
 export const Config = () => {
   const { user } = useAuth();
+  const [maximumBidAmount, setMaximumBidAmount] = useState(0);
+  const [bidAlertAt, setBidAlertAt] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      setMaximumBidAmount(user.maximum_bid_amount);
+      setBidAlertAt(user.bid_alert_at);
+    }
+  }, [user]);
+
+  const handleSaveConfigs = async () => {
+    setIsLoading(true);
+    await api.patch("user/config", {
+      maximum_bid_amount: maximumBidAmount,
+      bid_alert_at: bidAlertAt,
+    });
+    setIsLoading(false);
+  };
+
   return (
     <Layout>
       {user === null ? (
@@ -21,24 +43,29 @@ export const Config = () => {
           </p>
           <p>Be mindful of the concurrency issues with auto-bidding!</p>
           <Input
-            value={user.maximum_bid_amount}
+            value={maximumBidAmount}
             name='bid-amount'
             label={"$"}
             labelPosition='left'
             type='number'
+            onChange={(e) => setMaximumBidAmount(e.currentTarget.valueAsNumber)}
           />
 
           <h4>Bid Alert notification</h4>
           <p>Get the notification about your reserved bids</p>
           <Input
-            value={user.bid_alert_at}
+            value={bidAlertAt}
             name='porc-notification'
             label={"%"}
             labelPosition='right'
             type='number'
+            max='100'
+            onChange={(e) => setBidAlertAt(e.currentTarget.valueAsNumber)}
           />
 
-          <Button>Save</Button>
+          <Button onClick={handleSaveConfigs} disabled={isLoading}>
+            {isLoading ? "..." : "Save"}
+          </Button>
         </div>
       )}
     </Layout>
